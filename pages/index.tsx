bc22845cfbccce16013b53a733b6faa383ba69bb
NerdyDave2017 <ajayioluwasegun2018@gmail.com>
2023-02-28 23:08:44 +0100

    feat: add map component to index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,14 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
+import Map from "../components/map";
+
 const Home: NextPage = () => {
-  return <></>;
+  return (
+    <>
+      <Map />
+    </>
+  );
 };
 
 export default Home;
