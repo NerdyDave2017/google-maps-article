d6b5f1e09a60a497b5eb3cb8edf0984de59bdda0
NerdyDave2017 <ajayioluwasegun2018@gmail.com>
2023-03-04 15:08:08 +0100

    feat: add custom colours to tailwindcss.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      purple: {
+        default: "#8D6CEB",
+        light: "rgba(141, 108, 235, 0.3)",
+      },
+      white: "#fff",
+    },
     extend: {},
   },
   plugins: [require("flowbite/plugin")],
