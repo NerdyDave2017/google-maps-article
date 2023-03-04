aa44e2b1a85eaada7ca2eeb678594ecd6ce4c5a2
NerdyDave2017 <ajayioluwasegun2018@gmail.com>
2023-03-04 16:46:00 +0100

    feat: update IconButton component

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { FC, ReactNode } from "react";
 
-function IconButton() {
-  return <div></div>;
-}
+/**
+ * `IconButtonProps` is an object with a single property, `Icon`, which is a function that takes an
+ * `SVGProps` object and returns a `JSX.Element`.
+ * @property Icon - This is the icon that will be displayed in the button.
+ */
+type IconButtonProps = {
+  onClick?: () => void;
+  children?: ReactNode;
+};
+
+const IconButton = ({ onClick, children }: IconButtonProps) => {
+  return (
+    <div
+      className="w-12 h-12 lg:w-14 lg:h-14 bg-white text-purple-default rounded-2xl hover:bg-purple-default hover:text-white focus:outline-none"
+      onClick={onClick}
+    >
+      <div className="flex justify-center items-center h-full">{children}</div>
+    </div>
+  );
+};
 
 export default IconButton;
