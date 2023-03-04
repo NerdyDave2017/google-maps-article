532ed830fb21a19ec82b63990ac58fcf239bfbad
NerdyDave2017 <ajayioluwasegun2018@gmail.com>
2023-03-04 17:22:00 +0100

    feat: add material UI icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 import type { NextPage } from "next";
 
+import IconButton from "../components/IconButton";
+
 import Map from "../components/map";
 
 const Home: NextPage = () => {
   return (
     <>
+      <IconButton></IconButton>
       <Map />
     </>
   );
