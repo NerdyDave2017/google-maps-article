c687b858840d5e1dd7bfbef6abbedd735e1b72b7
NerdyDave2017 <ajayioluwasegun2018@gmail.com>
2023-03-04 19:00:55 +0100

    feat: finish UI layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+// import icons
 import MyLocationIcon from "@mui/icons-material/MyLocation";
+import SearchIcon from "@mui/icons-material/Search";
+import AddLocationAltIcon from "@mui/icons-material/AddLocationAlt";
+import LocalOfferIcon from "@mui/icons-material/LocalOffer";
+import TransferWithinAStationIcon from "@mui/icons-material/TransferWithinAStation";
+import ZoomInIcon from "@mui/icons-material/ZoomIn";
+import ZoomOutIcon from "@mui/icons-material/ZoomOut";
 
 import Map from "./map";
 import IconButton from "./IconButton";
@@ -13,11 +20,30 @@ const Layout = () => {
           <MyLocationIcon className="w-6 h-6 lg:w-7 lg:h-7" />
         </IconButton>
         <IconButton>
-          <MyLocationIcon className="w-6 h-6 lg:w-7 lg:h-7" />
+          <SearchIcon className="w-6 h-6 lg:w-7 lg:h-7" />
         </IconButton>
       </div>
       {/* Bottom Nav */}
-      <div className=""></div>
+      <div
+        className="flex w-auto rounded-2xl bg-white absolute z-50 bottom-20"
+        style={{ left: "50%", transform: "translateX(-50%)" }}
+      >
+        <IconButton>
+          <AddLocationAltIcon className="w-6 h-6 lg:w-7 lg:h-7" />
+        </IconButton>
+        <IconButton>
+          <LocalOfferIcon className="w-6 h-6 lg:w-7 lg:h-7" />
+        </IconButton>
+        <IconButton>
+          <TransferWithinAStationIcon className="w-6 h-6 lg:w-7 lg:h-7" />
+        </IconButton>
+        <IconButton>
+          <ZoomInIcon className="w-6 h-6 lg:w-7 lg:h-7" />
+        </IconButton>
+        <IconButton>
+          <ZoomOutIcon className="w-6 h-6 lg:w-7 lg:h-7" />
+        </IconButton>
+      </div>
       {/* GoogleMap */}
       <Map />
     </div>
