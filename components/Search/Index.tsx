fbd69a6dc8d7872bb2f2079aeacbdff5620de35c
NerdyDave2017 <ajayioluwasegun2018@gmail.com>
2023-03-05 02:14:12 +0100

    feat: update layout, search card components

--- components/Search/Index.tsx
+++ components/Search/Index.tsx
@@ -2,22 +2,25 @@ import React from "react";
 import SearchIcon from "@mui/icons-material/Search";
 
 import IconButton from "../IconButton";
-
 import SearchBar from "./SearchBar";
+import SearchCard from "./SearchCard";
 
 const Index = () => {
   return (
-    <div className="flex flex-1">
-      <SearchBar />
-      <IconButton
-        shadow={true}
-        style={{
-          borderTopLeftRadius: "0rem",
-          borderBottomLeftRadius: "0rem",
-        }}
-      >
-        <SearchIcon className="w-6 h-6 lg:w-7 lg:h-7" />
-      </IconButton>
+    <div className="flex-col space-y-1 flex-1">
+      <div className="flex">
+        <SearchBar />
+        <IconButton
+          shadow={true}
+          style={{
+            borderTopLeftRadius: "0rem",
+            borderBottomLeftRadius: "0rem",
+          }}
+        >
+          <SearchIcon className="w-6 h-6 lg:w-7 lg:h-7" />
+        </IconButton>
+      </div>
+      <SearchCard />
     </div>
   );
 };
--- components/Search/SearchCard.tsx
+++ components/Search/SearchCard.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const SearchCard = () => {
-  return <div>SearchCard</div>;
+  return (
+    <div
+      className="w-full h-12 lg:h-14 bg-white rounded-2xl px-3"
+      style={{ boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.25)" }}
+    ></div>
+  );
 };
 
 export default SearchCard;
