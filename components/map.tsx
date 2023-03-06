87ee094a5f6a47250f69dc83cbff69206db1d9b4
NerdyDave2017 <ajayioluwasegun2018@gmail.com>
2023-03-06 23:37:19 +0100

    feat: add marker component to map component and add marker

--- components/map.tsx
+++ components/map.tsx
@@ -8,6 +8,8 @@ import React, {
   useEffect,
 } from "react";
 
+import Marker from "./Marker";
+
 // Map Types
 type LatLngLiteral = google.maps.LatLngLiteral;
 type MapOptions = google.maps.MapOptions;
@@ -83,6 +85,7 @@ const Map = () => {
       onLoad={onLoad}
     >
       {/* Child components, such as markers, info windows, etc. */}
+      <Marker position={{ lat: 53.344250668504806, lng: -6.261668903294844 }} />
     </GoogleMap>
   );
 };
