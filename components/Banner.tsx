d6cd97f32f886935342e47b6794b528376512b98
NerdyDave2017 <ajayioluwasegun2018@gmail.com>
2023-03-07 01:02:44 +0100

    feat: create function to prompt banner to add marker

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { ReactNode, FC, useContext } from "react";
+import Spinner from "./Spinner";
+
+import GlobalVariableContext from "../context/GlobalVaribales";
 
 const Banner = () => {
-  return <div>Banner</div>;
+  const { bannerMessage } = useContext(GlobalVariableContext);
+
+  return (
+    <div className="w-full h-8 bg-purple-default text-white z-50 absolute top-0 flex justify-center items-center space-x-4">
+      <Spinner />
+      {bannerMessage}
+    </div>
+  );
 };
 
 export default Banner;
