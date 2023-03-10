a0a753180a78bf30fab1d72ac937e7b8e4a4b52d
NerdyDave2017 <ajayioluwasegun2018@gmail.com>
2023-03-10 21:40:49 +0100

    feat: create add overlay marker function

--- components/Overlay/Index.tsx
+++ components/Overlay/Index.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import LocalOfferIcon from "@mui/icons-material/LocalOffer";
 
 import IconButton from "../IconButton";
+import GlobalVariableContext from "../../context/GlobalVaribales";
 
 const Index = () => {
+  const { setShowBanner, setBannerMessage, setAddMarker } = useContext(
+    GlobalVariableContext
+  );
+
+  const addOverlayMarker = () => {
+    // Show banner
+    setShowBanner(true);
+    // Add custom banner message
+    setBannerMessage("Double click map to add Overlay Marker");
+    // set add marker value to true to allow adding new marker
+    setAddMarker(true);
+    // set marker type
+  };
+
   return (
-    <div>
+    <div onClick={addOverlayMarker}>
       <IconButton>
         <LocalOfferIcon className="w-6 h-6 lg:w-7 lg:h-7" />
       </IconButton>
